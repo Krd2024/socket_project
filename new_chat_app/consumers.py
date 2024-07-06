93c7a6a8becbbbee1457b7e78a4645b087ebe002
Krd2024 <161505798+Krd2024@users.noreply.github.com>
2024-07-06 23:28:40 +0300

    new-6

--- new_chat_app/consumers.py
+++ new_chat_app/consumers.py
@@ -16,26 +16,27 @@ from datetime import datetime
 from django.template.loader import render_to_string
 
 LIMIT_OF_USERS_ON_PAGE = 5
+# f"room_{self.room_id}_page_{self.num}
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self, **kwargs):
+        print("Connecting Connecting Connecting")
         self.accept()
-        room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        # self.room_id = self.room_id
         page = self.scope["url_route"]["kwargs"]["page_num"]
         query_string = self.scope["query_string"].decode()
 
-        self.room_group_name = f"chat_{room_id}"
-        #
+        self.room_group_name = f"chat_{self.room_id}"
+
         self.channel_layer.group_add(self.room_group_name, self.channel_name)
         query_params = parse_qs(query_string)
 
         self.user_id = query_params.get("user_id", [None])[0]
-        # room_id = kwargs.get('roomid')
-        room_obj = Room.objects.get(id=room_id)
+        room_obj = Room.objects.get(id=self.room_id)
         messages = room_obj.messages.all()
         # =================================================================
-        print(page, "< --- page")
         try:
             if page is not None:
                 try:
@@ -80,7 +81,7 @@ class ChatConsumer(WebsocketConsumer):
             sorted_users = paginator.page(paginator.num_pages)
 
         context = {
-            "room_id": room_id,
+            "room_id": self.room_id,
             "page_number": page,
             "pages_range": page_range,
             "messages": sorted_users,
@@ -96,39 +97,47 @@ class ChatConsumer(WebsocketConsumer):
 
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        print(message, " < ------------------")
 
         room_id = self.scope["url_route"]["kwargs"]["room_id"]
 
         room, x = Room.objects.get_or_create(id=room_id)
         user, y = User.objects.get_or_create(id=self.user_id)
-        print(user, room)
+        # print(user, room)
 
-        add = user.rooms.add(room)
-        print(add)
+        user.rooms.add(room)
         room.add_message(user, message)
         room.add_user(user)
-
-        x = self.channel_layer.group_send(
+        # html_string_2 = render_to_string(
+        #     # "new_chat_app/live_messages.html", {"message": message}
+        #     "new_chat_app/messages.html",
+        #     {"message": message},
+        # )
+        # self.send(text_data=html_string_2)
+        self.channel_layer.group_send(
             self.room_group_name,
             {"type": "chat.message", "message": message},
         )
+        print(x)
         self.send(text_data=json.dumps({"message": message}))
-        # print(x)
 
     # def chat_message(self, event=None):
-    #     print(event, "<------- event")
+    #     print("Chat message < ------------------")
     #     message = event["message"]
+
+    #     print(f"room name: {self.room_id} ")
+
+    #     self.send(text_data=json.dumps({"message": message}))
+
+    # def chat_message(self, event=None):
+    # print(event, "<------- event")
+    # message = event["message"]
     #     print(message, "<------ message")
 
     # context = {"message": message}
     # html_string = render_to_string("new_chat_app/messages.html", context)
     # self.send(text_data=html_string)
-    # self.send(text_data=json.dumps({"message": message}))
+
     #
 
-    # async def chat_message(self, event=None):
-    #     message = event["message"]
-
-    #     print(f"room name: {self.room_name} ")
-    #     await self.send(text_data=json.dumps({"message": message}))
-    #     #
+    #
